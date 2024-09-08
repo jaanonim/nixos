fe56022deb81d77b99005ec8df3150aa20c9bf74
jaanonim <mat8mro@gmail.com>
2024-09-08 22:22:18 +0200

    fix disks

--- common/nix/optional/disks.nix
+++ common/nix/optional/disks.nix
@@ -1,8 +1,8 @@
-_:{
-  boot.supportedFilesystems = [ "ntfs" ];
-   fileSystems."/mnt/dane" =
-    { device = "/dev/nvme1n1p2";
-      fsType = "ntfs-3g"; 
-      options = [ "rw" "uid=1000"];
-    };
+_: {
+  boot.supportedFilesystems = ["ntfs"];
+  fileSystems."/mnt/dane" = {
+    device = "/dev/nvme1n1p2";
+    fsType = "ntfs-3g";
+    options = ["rw" "uid=1000" "gid=1000" "umask=0022" "auto"];
+  };
 }
