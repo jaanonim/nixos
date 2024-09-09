c4f42f829cf0f9a8abafe5056ffdf783d4d74137
jaanonim <mat8mro@gmail.com>
2024-09-09 16:10:19 +0200

    set kscreenlocker.timeout

--- common/home/optional/plasma/powerdevil.nix
+++ common/home/optional/plasma/powerdevil.nix
@@ -1,44 +1,47 @@
 _: {
-  programs.plasma.powerdevil = {
-    AC = {
-      powerButtonAction = "shutDown";
-      autoSuspend.action = "nothing";
-      whenLaptopLidClosed = "turnOffScreen";
-      turnOffDisplay = {
-        idleTimeout = 3600;
-        idleTimeoutWhenLocked = 600;
+  programs.plasma = {
+    kscreenlocker.timeout = 15;
+    powerdevil = {
+      AC = {
+        powerButtonAction = "shutDown";
+        autoSuspend.action = "nothing";
+        whenLaptopLidClosed = "turnOffScreen";
+        turnOffDisplay = {
+          idleTimeout = 3600;
+          idleTimeoutWhenLocked = 600;
+        };
       };
-    };
-    battery = {
-      powerButtonAction = "shutDown";
-      autoSuspend = {
-        action = "sleep";
-        idleTimeout = 1800;
+      battery = {
+        powerButtonAction = "shutDown";
+        autoSuspend = {
+          action = "sleep";
+          idleTimeout = 1800;
+        };
+        whenLaptopLidClosed = "sleep";
+        turnOffDisplay = {
+          idleTimeout = 900;
+          idleTimeoutWhenLocked = 300;
+        };
+        dimDisplay = {
+          enable = true;
+          idleTimeOut = 600;
+        };
       };
-      whenLaptopLidClosed = "sleep";
-      turnOffDisplay = {
-        idleTimeout = 900;
-        idleTimeoutWhenLocked = 300;
-      };
-      dimDisplay = {
-        enable = true;
-        idleTimeOut = 600;
-      };
-    };
-    lowBattery = {
-      powerButtonAction = "shutDown";
-      autoSuspend = {
-        action = "hibernate";
-        idleTimeout = 900;
-      };
-      whenLaptopLidClosed = "sleep";
-      turnOffDisplay = {
-        idleTimeout = 600;
-        idleTimeoutWhenLocked = 300;
-      };
-      dimDisplay = {
-        enable = true;
-        idleTimeOut = 180;
+      lowBattery = {
+        powerButtonAction = "shutDown";
+        autoSuspend = {
+          action = "hibernate";
+          idleTimeout = 900;
+        };
+        whenLaptopLidClosed = "sleep";
+        turnOffDisplay = {
+          idleTimeout = 600;
+          idleTimeoutWhenLocked = 300;
+        };
+        dimDisplay = {
+          enable = true;
+          idleTimeOut = 180;
+        };
       };
     };
   };
