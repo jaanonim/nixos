c3642ff430e2c50e7ab90b1ba71a09b62e8836fb
jaanonim <mat8mro@gmail.com>
2024-09-11 15:40:26 +0200

    fix pollmc

--- common/nix/optional/apps/gaming.nix
+++ common/nix/optional/apps/gaming.nix
@@ -1,12 +1,7 @@
-{
-  pkgs,
-  jaanonim-pkgs,
-  ...
-}: {
-  packages = with pkgs;
-    [
-      steam
-      (lutris.override {extraLibraries = pkgs: [pkgs.libunwind];})
-    ]
-    ++ (with jaanonim-pkgs; [pollymc]);
+{pkgs, ...}: {
+  packages = with pkgs; [
+    steam
+    (lutris.override {extraLibraries = pkgs: [pkgs.libunwind];})
+    prismlauncher
+  ];
 }
