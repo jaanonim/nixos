eabeae648766a91a3e1b49f07622dfd57a7a89f5
jaanonim <mat8mro@gmail.com>
2024-09-22 15:40:03 +0200

    fix disk mounting

--- common/nix/optional/disks.nix
+++ common/nix/optional/disks.nix
@@ -1,7 +1,7 @@
 _: {
   boot.supportedFilesystems = ["ntfs"];
   fileSystems."/mnt/dane" = {
-    device = "/dev/nvme1n1p2";
+    device = "/dev/disk/by-uuid/0C2A2C6F2A2C57CA";
     fsType = "ntfs-3g";
     options = ["rw" "uid=1000" "gid=1000" "umask=0022" "auto"];
   };
