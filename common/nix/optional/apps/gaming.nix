7eff5f893c52f12ca30373db562f8cef44dca730
jaanonim <mat8mro@gmail.com>
2024-10-14 12:28:29 +0200

    fix lutris umu

--- common/nix/optional/apps/gaming.nix
+++ common/nix/optional/apps/gaming.nix
@@ -1,7 +1,10 @@
 {pkgs, ...}: {
   packages = with pkgs; [
     steam
-    (lutris.override {extraLibraries = pkgs: [pkgs.libunwind];})
+    (lutris.override {
+      extraLibraries = pkgs: [pkgs.libunwind];
+      extraPkgs = pkgs: [pkgs.python3];
+    })
     prismlauncher
   ];
 }
