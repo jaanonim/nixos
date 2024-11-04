6800289a62b1c1211466fa9ca2c3da135eb9af9f
jaanonim <mat8mro@gmail.com>
2024-11-04 23:26:57 +0100

    lock to older version of prismlauncher

--- common/nix/optional/apps/gaming.nix
+++ common/nix/optional/apps/gaming.nix
@@ -1,10 +1,16 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  myPrismlauncher =
+    (import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/05bbf675397d5366259409139039af8077d695ce.tar.gz";
+    }) {})
+    .prismlauncher;
+in {
   programs.steam.enable = true;
   packages = with pkgs; [
     (lutris.override {
       extraLibraries = pkgs: [pkgs.libunwind];
       extraPkgs = pkgs: [pkgs.python3];
     })
-    prismlauncher
+    myPrismlauncher
   ];
 }
