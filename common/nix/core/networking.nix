19c0d522605d285afa3b3ecd76d827b961f462f6
jaanonim <mat8mro@gmail.com>
2025-01-13 22:48:00 +0100

    dns changes

--- common/nix/core/networking.nix
+++ common/nix/core/networking.nix
@@ -2,6 +2,6 @@ _: {
   networking = {
     networkmanager.enable = true;
     firewall.enable = true;
-    nameservers = ["192.168.1.150" "1.1.1.1"];
+    nameservers = ["192.168.1.150" "1.1.1.1" "1.0.0.1"];
   };
 }
