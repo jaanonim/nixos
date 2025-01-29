83c3343666e3d0efd1aa0edce7ea08725910f5a2
jaanonim <mat8mro@gmail.com>
2025-01-30 00:19:43 +0100

    switch to discord canary

--- common/nix/optional/apps/discord.nix
+++ common/nix/optional/apps/discord.nix
@@ -4,12 +4,7 @@
   ...
 }: {
   packages = [
-    # TODO: This override is not working
-    (pkgs.discord.overrideAttrs (oldAttrs: {
-      desktopItem = oldAttrs.desktopItem.override {
-        exec = "env XDG_SESSION_TYPE=x11 Discord";
-      };
-    }))
+    pkgs.discord-canary
   ];
 
   environment.etc."xdg/autostart/discord.desktop".source = "${configLib.get_util "discord-autostart" {inherit pkgs;}}/share/applications/discord.desktop";
--- pkgs/utils/discord-autostart.nix
+++ pkgs/utils/discord-autostart.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}:
 pkgs.runCommand "discord-autostart" {} ''
   mkdir -p $out/share/applications
-  substitute ${pkgs.discord}/share/applications/discord.desktop $out/share/applications/discord.desktop \
+  substitute ${pkgs.discord-canary}/share/applications/discord-canary.desktop $out/share/applications/discord.desktop \
     --replace "Exec=Discord" "Exec=env XDG_SESSION_TYPE=x11 Discord --start-minimized"
 ''
