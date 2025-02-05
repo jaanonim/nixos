d48f2eba89b6e7a1cfd7c349ccb3c1e398c8f0f4
jaanonim <mat8mro@gmail.com>
2025-02-05 12:04:56 +0100

    prismlauncher update and gamescope for steam

--- common/nix/optional/apps/gaming.nix
+++ common/nix/optional/apps/gaming.nix
@@ -1,17 +1,16 @@
-{pkgs, ...}: let
-  myPrismlauncher =
-    (import (builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/05bbf675397d5366259409139039af8077d695ce.tar.gz";
-      sha256 = "1r26vjqmzgphfnby5lkfihz6i3y70hq84bpkwd43qjjvgxkcyki0";
-    }) {})
-    .prismlauncher;
-in {
-  programs.steam.enable = true;
+{pkgs, ...}: {
+  programs = {
+    gamescope.enable = true;
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+  };
   packages = with pkgs; [
     (lutris.override {
       extraLibraries = pkgs: [pkgs.libunwind];
       extraPkgs = pkgs: [pkgs.python3];
     })
-    myPrismlauncher
+    prismlauncher
   ];
 }
