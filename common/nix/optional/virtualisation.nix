c4c83df73bb76449e8bd4490c4bfde26575f8f57
jaanonim <mat8mro@gmail.com>
2025-02-05 01:48:43 +0100

    docker nvidia toolkit

--- common/nix/optional/virtualisation.nix
+++ common/nix/optional/virtualisation.nix
@@ -1,6 +1,11 @@
 _: {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    enableNvidia = true;
+    autoPrune.enable = true;
+  };
   users.extraGroups.docker.members = ["jaanonim"];
+  hardware.nvidia-container-toolkit.enable = true;
 
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = ["jaanonim"];
