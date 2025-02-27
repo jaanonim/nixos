ea9a63548b2fcef800f2682737c0bc1946317fb8
jaanonim <mat8mro@gmail.com>
2025-02-27 12:55:27 +0100

    remove openvpn

--- common/nix/optional/vpn.nix
+++ common/nix/optional/vpn.nix
@@ -1,12 +1,3 @@
 _: {
   services.tailscale.enable = true;
-  services.openvpn = {
-    # servers = {
-    #   AGH = {
-    #     config = ''config /home/jaanonim/VPN-AGH.2024.ovpn '';
-    #     updateResolvConf = true;
-    #     autoStart = false;
-    #   };
-    # };
-  };
 }
