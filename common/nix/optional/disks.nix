3eb92c701d6e1f207f47a42082e58fa4f8fb06b0
jaanonim <mat8mro@gmail.com>
2025-05-05 10:53:30 +0200

    fix exec perm on disk dane

--- common/nix/optional/disks.nix
+++ common/nix/optional/disks.nix
@@ -2,6 +2,6 @@ _: {
   fileSystems."/mnt/dane" = {
     device = "/dev/disk/by-uuid/ee53362c-cad2-4ac1-9060-02c868233572";
     fsType = "ext4";
-    options = ["users" "nofail"];
+    options = ["users" "nofail" "exec"];
   };
 }
