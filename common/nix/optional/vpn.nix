492379f82721b2600e05163c285ec618db8e6062
jaanonim <mat8mro@gmail.com>
2025-05-12 12:34:56 +0200

    vpn autostart

--- common/nix/optional/vpn.nix
+++ common/nix/optional/vpn.nix
@@ -4,4 +4,5 @@
     extraSetFlags = ["--accept-routes=true" "--operator=jaanonim"];
   };
   environment.systemPackages = with pkgs; [tail-tray];
+  environment.etc."xdg/autostart/tail-tray.desktop".source = "${pkgs.tail-tray}/share/applications/tail-tray.desktop";
 }
