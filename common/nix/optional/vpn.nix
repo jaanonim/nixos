f8c25c2f4a04c1b514619fa855677f0c1c73fb94
jaanonim <mat8mro@gmail.com>
2025-06-18 09:33:07 +0200

    tail-tray breaks kde connect

--- common/nix/optional/vpn.nix
+++ common/nix/optional/vpn.nix
@@ -3,6 +3,6 @@
     enable = true;
     extraSetFlags = ["--accept-routes=true" "--operator=jaanonim"];
   };
-  environment.systemPackages = with pkgs; [tail-tray];
-  environment.etc."xdg/autostart/tail-tray.desktop".source = "${pkgs.tail-tray}/share/applications/tail-tray.desktop";
+  # environment.systemPackages = with pkgs; [tail-tray];
+  # environment.etc."xdg/autostart/tail-tray.desktop".source = "${pkgs.tail-tray}/share/applications/tail-tray.desktop";
 }
