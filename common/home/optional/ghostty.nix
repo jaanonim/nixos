f3ac347422e0484e4790145df279711aa0c36101
jaanonim <mat8mro@gmail.com>
2025-06-21 12:45:34 +0200

    small update

--- common/home/optional/ghostty.nix
+++ common/home/optional/ghostty.nix
@@ -6,7 +6,7 @@ _: {
       cursor-style = "block";
       cursor-style-blink = false;
       shell-integration-features = "no-cursor";
-      background-opacity = 0.2;
+      background-opacity = 0.5;
       background-blur = true;
       working-directory = "/home/jaanonim/Pobrane";
     };
