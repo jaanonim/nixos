27dbea00a5d89c7d2ea05a1b0c102152e7f0fac7
jaanonim <mat8mro@gmail.com>
2025-07-21 00:07:46 +0200

    domain config

--- modules/containers/default.nix
+++ modules/containers/default.nix
@@ -1,3 +1,28 @@
-_: {
+{
+  lib,
+  config,
+  ...
+}:
+with lib; {
   imports = builtins.map (path: ./. + /${path}) (builtins.attrNames (builtins.removeAttrs (builtins.readDir ./.) ["default.nix"]));
+
+  options.my.containers = {
+    domain = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      example = "server.local";
+      description = "Domain name for this host";
+    };
+    _domain = mkOption {
+      type = types.str;
+      description = "Internal don't change";
+    };
+  };
+
+  config = {
+    my.containers._domain =
+      if domain == null
+      then "${config.my.hostname}.local"
+      else domain;
+  };
 }
