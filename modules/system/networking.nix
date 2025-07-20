e70b77bbdb54185d6564d14f4307556b5c98c604
jaanonim <mat8mro@gmail.com>
2025-07-21 00:07:31 +0200

    static ip module

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -9,12 +9,35 @@ with lib; let
   cfg = config.my.networking;
 in {
   options.my.networking = {
-    networkmanager = mkEnableOption "Network manager to control networking settings";
+    networkmanager = mkEnableOption "Network manager to control networking settings (if not enabled dhcp and static configuration will be used)";
     firewall = mkEnableOption "Firewall";
     dns = mkOption {
       type = types.listOf types.str;
       default = ["1.1.1.1" "1.0.0.1"];
+      example = ["8.8.8.8"];
       description = "List of DNS servers to use";
+    };
+    mask = mkOption {
+      type = types.int;
+      default = 24;
+      example = 16;
+      description = "Mask for specified ip";
+    };
+    interface = mkOption {
+      type = types.nullOr types.str;
+      example = "ens1";
+      description = "Interface that will have static ip";
+    };
+    ip = mkOption {
+      type = types.str;
+      example = "192.168.1.5";
+      description = "Static IPv4 for specified interface";
+    };
+    gateway = mkOption {
+      type = types.str;
+      default = "192.168.1.1";
+      example = "192.168.0.1";
+      description = "Default gateway for specified interface";
     };
   };
 
@@ -24,6 +47,23 @@ in {
       firewall.enable = cfg.firewall;
       nameservers = cfg.dns;
       hostName = my.hostname;
+      useDHCP = !cfg.networkmanager;
+
+      interfaces = mkIf (!cfg.networkmanager) {
+        ${cfg.interface} = {
+          ipv4.addresses = [
+            {
+              address = cfg.ip;
+              prefixLength = cfg.mask;
+            }
+          ];
+        };
+      };
+
+      defaultGateway = mkIf (!cfg.networkmanager) {
+        address = cfg.gateway;
+        inherit (cfg) interface;
+      };
     };
 
     systemd.services.NetworkManager-wait-online.enable = my.boot.optimize;
