cedea8dd378628afefe60660af69b6e297b85f28
jaanonim <mat8mro@gmail.com>
2025-09-02 00:17:25 +0200

    tailscale auth and routes

--- modules/system/vpn.nix
+++ modules/system/vpn.nix
@@ -6,6 +6,7 @@
 }:
 with lib; let
   cfg = config.my.vpn.tailscale;
+  inherit (config) my;
 in {
   options.my.vpn.tailscale = {
     enable = mkEnableOption "Tailscale vpn";
@@ -14,13 +15,38 @@ in {
       default = false;
       description = "Add tail-tray GUI for tailscale";
     };
+    useAuthKey = mkEnableOption "auth key form sops";
+    advertiseRoutes = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = "List of CIDRs to be advertised";
+      default = [];
+      example = ["192.168.0.0/24"];
+    };
   };
 
   config = mkIf cfg.enable {
-    services.tailscale = {
-      enable = true;
-      extraSetFlags = ["--accept-routes=true" "--operator=jaanonim"];
-    };
+    assertions = [
+      {
+        assertion = cfg.useAuthKey -> my.sops;
+        message = "to use auth key, sops need to be enabled";
+      }
+    ];
+
+    sops.secrets = mkIf cfg.useAuthKey {tailscale-auth-key = {};};
+
+    services.tailscale =
+      {
+        enable = true;
+        extraSetFlags = ["--accept-routes=true" "--operator=${my.mainUser}"];
+      }
+      // lib.optionalAttrs cfg.useAuthKey {
+        authKeyFile = config.sops.secrets.tailscale-auth-key.path;
+      }
+      // lib.optionalAttrs (builtins.length cfg.advertiseRoutes > 0) {
+        extraUpFlags = [
+          ("--advertise-routes=" + (builtins.concatStringsSep "," cfg.advertiseRoutes))
+        ];
+      };
 
     environment = mkIf cfg.withTailTray {
       systemPackages = with pkgs; [tail-tray];
