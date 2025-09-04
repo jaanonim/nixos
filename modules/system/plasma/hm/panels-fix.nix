af67d278b84129d3078220f5f81d2024fbc899c6
jaanonim <mat8mro@gmail.com>
2025-09-04 11:43:58 +0200

    fix for fix

--- modules/system/plasma/hm/panels-fix.nix
+++ modules/system/plasma/hm/panels-fix.nix
@@ -1,3 +1,17 @@
-{lib, ...}: {
-  home.file.".config/plasma-org.kde.plasma.desktop-appletsrc".source = lib.root /config/plasma-org.kde.plasma.desktop-appletsrc;
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  pkg = pkgs.stdenv.mkDerivation {
+    name = "plasma-desktop-appletsrc";
+    src = lib.root /config;
+    installPhase = ''
+      mkdir -p $out
+      cp plasma-org.kde.plasma.desktop-appletsrc $out/plasma-org.kde.plasma.desktop-appletsrc
+      sed -i"" 's,@@ICON@@,${lib.profileImage}/profile.png,g' $out/plasma-org.kde.plasma.desktop-appletsrc
+    '';
+  };
+in {
+  home.file.".config/plasma-org.kde.plasma.desktop-appletsrc".source = "${pkg}/plasma-org.kde.plasma.desktop-appletsrc";
 }
