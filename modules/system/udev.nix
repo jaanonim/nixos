11ab0ae9c0cce496326233935776f8e50c1c507c
jaanonim <mat8mro@gmail.com>
2025-10-10 16:36:41 +0200

    better udev

--- modules/system/udev.nix
+++ modules/system/udev.nix
@@ -5,16 +5,34 @@
 }:
 with lib; let
   cfg = config.my.udev;
-in {
-  options.my.udev = {
-    enable = mkEnableOption "Extra udev rules";
-  };
-
-  config = mkIf cfg.enable {
-    hardware.probe-rs.enable = true;
-    services.udev.extraRules = ''
+  udevRules = {
+    attiny85 = ''
       SUBSYSTEMS=="usb", ATTRS{idVendor}=="16d0", ATTRS{idProduct}=="0753", MODE:="0666"
       KERNEL=="ttyACM*", ATTRS{idVendor}=="16d0", ATTRS{idProduct}=="0753", MODE:="0666", ENV{ID_MM_DEVICE_IGNORE}="1"
     '';
+    esp32 = ''
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE="0666"
+    '';
+  };
+in {
+  options.my.udev = {
+    enable = mkEnableOption "Extra udev rules";
+    probe-rs = mkOption {
+      type = types.bool;
+      example = false;
+      default = true;
+      description = "Whether to enable udev rules for rs-probe";
+    };
+    rules = mkOption {
+      type = types.listOf (types.enum (builtins.attrNames udevRules));
+      example = ["esp32"];
+      default = builtins.attrNames udevRules;
+      description = "For witch devices to enable udev rules";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    hardware.probe-rs.enable = cfg.probe-rs;
+    services.udev.extraRules = builtins.concatStringsSep "\n" (builtins.map (name: udevRules.${name}) cfg.rules);
   };
 }
