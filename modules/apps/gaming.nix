5c2469231026d07f0707f2b16f14c21d8451466c
jaanonim <mat8mro@gmail.com>
2025-11-02 21:43:05 +0100

    lutris -> heroic

--- modules/apps/gaming.nix
+++ modules/apps/gaming.nix
@@ -9,11 +9,8 @@ with lib; let
 in {
   config = mkIf (builtins.any (ele: (ele == (lib.removeSuffix ".nix" (baseNameOf __curPos.file)))) my.apps) {
     my._packages = with pkgs; [
-      (lutris.override {
-        extraLibraries = pkgs: [pkgs.libunwind];
-        extraPkgs = pkgs: [pkgs.python3];
-      })
       prismlauncher
+      heroic
     ];
 
     programs = {
