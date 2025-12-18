d95a6803f0fd41492b1e2472483f1f0af54952cd
jaanonim <mat8mro@gmail.com>
2025-12-18 21:41:54 +0100

    vencord

--- modules/apps/discord.nix
+++ modules/apps/discord.nix
@@ -6,16 +6,19 @@
 }:
 with lib; let
   inherit (config) my;
+  discordPkg = pkgs.discord.override {
+    withVencord = true;
+  };
 in {
   config = mkIf (builtins.any (ele: (ele == (lib.removeSuffix ".nix" (baseNameOf __curPos.file)))) my.apps) {
     my._packages = [
-      pkgs.discord
+      discordPkg
     ];
 
     environment.etc."xdg/autostart/discord.desktop".source = let
       discord-autostart = pkgs.runCommand "discord-autostart" {} ''
         mkdir -p $out/share/applications
-        substitute ${pkgs.discord}/share/applications/discord.desktop $out/share/applications/discord.desktop \
+        substitute ${discordPkg}/share/applications/discord.desktop $out/share/applications/discord.desktop \
           --replace "Exec=Discord" "Exec=Discord --start-minimized"
       '';
     in "${discord-autostart}/share/applications/discord.desktop";
