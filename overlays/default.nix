470e20ecb6379e65ff0e7cb7e9acdf8cc6b4334c
jaanonim <mat8mro@gmail.com>
2026-05-04 14:38:03 +0200

    allow all for nixos firewall

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,1 +1,7 @@
-_: {}
+_: {
+  modifications = final: prev: {
+    nixos-firewall-tool = prev.nixos-firewall-tool.overrideAttrs (oldAttrs: {
+      patches = (oldAttrs.patches or []) ++ [./patches/nixos-firewall-tool.patch];
+    });
+  };
+}
