62747513cec20baf411e302935cdffd17a6da9e9
jaanonim <mat8mro@gmail.com>
2026-05-05 16:52:10 +0200

    new syncthing device

--- modules/apps/syncthing.nix
+++ modules/apps/syncthing.nix
@@ -21,11 +21,12 @@ in {
       settings = {
         devices = {
           "s8" = {id = "WZ5YX2T-HXPRSEN-5MIJN5J-K6GWYJS-FVEAOUE-WLS7B3Y-DF5O2V3-HFOM3QY";};
+          "pixel10" = {id = "2NPCC4C-RDOXALY-IM2XHIJ-LOBSW5V-FE5WHAQ-GN5LP6X-JSA4WXF-TH5TIQT";};
         };
         folders = {
           "obsydian-jaanonim" = {
             path = "/mnt/dane/Obsydian/jaanonim/";
-            devices = ["s8"];
+            devices = ["s8" "pixel10"];
             versioning = {
               type = "simple";
               params.keep = "30";
