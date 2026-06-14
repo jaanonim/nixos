768f41bdb218c37848e71b7cbbb6ddb38f6cd805
jaanonim <mat8mro@gmail.com>
2026-06-14 23:01:32 +0200

    university - zotero

---
+++ modules/apps/university.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  inherit (config) my;
+in {
+  config = mkIf (builtins.any (ele: (ele == (lib.removeSuffix ".nix" (baseNameOf __curPos.file)))) my.apps) {
+    my._packages = with pkgs; [
+      zotero
+    ];
+  };
+}
