20a8d946ea5df9216ff73b2b607b66ddcd2d56a3
jaanonim <mat8mro@gmail.com>
2026-09-07 18:11:11 +0200

    fix: broken vscode

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,5 +3,13 @@ _: {
     nixos-firewall-tool = prev.nixos-firewall-tool.overrideAttrs (oldAttrs: {
       patches = (oldAttrs.patches or []) ++ [./patches/nixos-firewall-tool.patch];
     });
+    # https://github.com/NixOS/nixpkgs/issues/560776
+    vscode = prev.vscode.overrideAttrs (old: {
+      postPatch =
+        old.postPatch
+        + ''
+          ln -s node_modules resources/app/node_modules.asar.unpacked
+        '';
+    });
   };
 }
